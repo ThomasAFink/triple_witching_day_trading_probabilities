fb777690e83f3d78cc1576ca150d5fe3c19c8119
thomasafink <thomas@thomasafink.com>
2023-09-20 13:07:50 +0200

    Add stockchart

--- stockchart.py
+++ stockchart.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date, timedelta
 
 # Reload the data since the environment was reset
 OS_PATH = os.path.dirname(os.path.realpath('__file__'))
@@ -45,3 +46,15 @@ fig.tight_layout()
 plt.savefig(os.path.join(OS_PATH, f'output/{ticker_symbol}_combined_data_since_1990_stockchart.jpg'), dpi=300)
 
 plt.show()
+
+
+# Given that we have data up to the end date, let's find the next TWD after the end date
+current_date = end_date
+
+# Increment by one day and check if the date meets the TWD criteria. Repeat until we find the next TWD.
+while True:
+    current_date += timedelta(days=1)
+    if current_date.weekday() == 4 and 15 <= current_date.day <= 21 and current_date.month in [3, 6, 9, 12]:
+        next_twd = current_date
+        break
+print(f"Next TWD is on: {next_twd}")
